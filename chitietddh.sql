a3fd602776ffe5e823af3016ef542200a1d872b4
baobaote00 <baobaote00@users.noreply.github.com>
2020-08-03 13:57:13 +0700

    :heart:

---
+++ chitietddh.sql
@@ -0,0 +1,43 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.9
+-- https://www.phpmyadmin.net/
+--
+-- Máy chủ: 127.0.0.1:3306
+-- Thời gian đã tạo: Th8 03, 2020 lúc 06:55 AM
+-- Phiên bản máy phục vụ: 5.7.21
+-- Phiên bản PHP: 5.6.35
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Cơ sở dữ liệu: `quanlybanhang`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `chitietddh`
+--
+
+DROP TABLE IF EXISTS `chitietddh`;
+CREATE TABLE IF NOT EXISTS `chitietddh` (
+  `MaDDH` char(5) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `MaMH` int(11) NOT NULL,
+  `SoLuong` int(11) NOT NULL,
+  `GiamGia` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`MaDDH`,`MaMH`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
